78620f7cc8646791e6784aaee2ba0c98bbfd0cf9
zrzf <zrzf@126.com>
2019-08-13 09:29:21 +0800

    提交

--- src/main/java/com/example/demo/config/KptchaConfig.java
+++ src/main/java/com/example/demo/config/KptchaConfig.java
@@ -1,4 +1,23 @@
 package com.example.demo.config;
 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
 public class KptchaConfig {
+    @Bean
+    public DefaultKaptcha getKptcha(){
+        Properties properties = new Properties();
+        properties.put("kaptcha.border","yes");
+        properties.put("kaptcha.textproducer.font.color","black");
+        properties.put("kaptcha.textproducer.char.space","5");
+        Config config = new Config(properties);
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+    }
 }
--- src/main/java/com/example/demo/web/KaptchaController.java
+++ src/main/java/com/example/demo/web/KaptchaController.java
@@ -1,4 +1,35 @@
 package com.example.demo.web;
 
+import com.google.code.kaptcha.Producer;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@RestController
 public class KaptchaController {
+    @Autowired
+    private Producer producer;
+    @PostMapping("/getCode")
+    public void getCode(HttpServletResponse response){
+        response.setHeader("Cache-Control","no-store, no-cache");
+        response.setContentType("image/jpeg");
+        try {
+            String code = producer.createText();
+            BufferedImage image = producer.createImage(code);
+            ServletOutputStream out = response.getOutputStream();
+            ImageIO.write(image,"jpg",out);
+            //关闭IO
+            IOUtils.closeQuietly(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
