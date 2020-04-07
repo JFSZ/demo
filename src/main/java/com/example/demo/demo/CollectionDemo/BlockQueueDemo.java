21bff4e0a56e44a37caae15860f903d575e67914
jfsz <843400447@qq.com>
2020-04-07 17:58:46 +0800

    CountDownLatch demo提交

--- src/main/java/com/example/demo/demo/CollectionDemo/BlockQueueDemo.java
+++ src/main/java/com/example/demo/demo/CollectionDemo/BlockQueueDemo.java
@@ -1,10 +1,55 @@
 package com.example.demo.demo.CollectionDemo;
 
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @description: 队列练习 队列分类。实现原理、常用方法。
  * @author: chenxue
  * @create: 2020-04-06 17:58
  **/
 public class BlockQueueDemo {
+    public static void main(String[] args) {
+        // 指定容器大小，并且默认是非公平锁 底层使用Lock锁，实现同步
+        ArrayBlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<>(20);
+        for (int i = 1; i <= 20; i++) {
+            arrayBlockingQueue.offer("" + i);
+        }
+        System.out.println(arrayBlockingQueue.size());
+        //offer() 此方法不允许添加null。 方法会在队列尾部添加元素，如果队列已满，则返回false,添加成功则返回true 。和add() 区别：add方法如果添加失败会抛出错误 IllegalStateException
+        // offer(E e,Long timeout,TimeUnit timeUnit) 在队列尾部插入元素e ,如果队列已满则阻塞线程 timeout 时间。底层调用lock.lockInterruptibly()可以响应中断.
+        //add方法，底层调用的是offer()
+        //boolean offer = arrayBlockingQueue.offer("21");
+        try {
+            boolean offer = arrayBlockingQueue.offer("22", 1L, TimeUnit.SECONDS);
+            System.out.println(offer);
+            // 将指定的元素插入此队列的尾部，如果该队列已满，则等待可用的空间。会让线程进入等待
+            //arrayBlockingQueue.put("23");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //boolean add = arrayBlockingQueue.add("21");
+        //System.out.println(add);
+        //System.out.println(offer);
+        //poll 获取并移除队列头部元素，如果队列为空则返回null
+        String poll = arrayBlockingQueue.poll();
+        System.out.println(poll + " == " + arrayBlockingQueue.size());
+        // peek 获取但不移除队列头部元素，如果队列为空则返回null
+        String peek = arrayBlockingQueue.peek();
+        System.out.println(peek);
+        ArrayList<String> list = new ArrayList<>();
+        // drainTo 把队列中所有元素移除，并放置在给定的集合Collection中
+        arrayBlockingQueue.drainTo(list);
+        System.out.println(list.toString());
+        System.out.println(arrayBlockingQueue.size());
+        //remainingCapacity 返回队列剩余可用容量
+        System.out.println(arrayBlockingQueue.remainingCapacity());
 
+        // 从此队列中移除指定元素的单个实例（如果存在）。 成功返回true 失败返回false
+        boolean remove = arrayBlockingQueue.remove("2");
+        System.out.println(remove);
+
+
+    }
 }
--- src/main/java/com/example/demo/demo/lock/CountDownLatchDemo.java
+++ src/main/java/com/example/demo/demo/lock/CountDownLatchDemo.java
@@ -1,9 +1,52 @@
 package com.example.demo.demo.lock;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @description: 并发工具类 一般用于某个线程A等待若干个其他线程执行完任务之后，它才执行
+ * 线程池 记得随手关闭。否则容易造成OOM
  * @author: chenxue
  * @create: 2020-04-06 18:06
  **/
 public class CountDownLatchDemo {
+    public static void main(String[] args) {
+        System.out.println("主线程开始");
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> {
+            //doWorking
+            try {
+                //模拟线程耗时操作
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("子线程" + Thread.currentThread().getName() + "开始执行");
+            countDownLatch.countDown();
+        });
+        executorService.shutdown();
+
+        ExecutorService executorService1 = Executors.newSingleThreadExecutor();
+        executorService1.execute(() -> {
+            //doWorking
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("子线程" + Thread.currentThread().getName() + "开始执行");
+            countDownLatch.countDown();
+        });
+        executorService1.shutdown();
+        try {
+            //让主线程等待其他线程完成
+            System.out.println("主线程--等待其他线程完毕，才能执行");
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("所有线程都执行完毕");
+    }
 }
