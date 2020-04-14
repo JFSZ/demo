82ecbe8234395d788a2182b9f4764b2db4d28eac
jfsz <843400447@qq.com>
2020-04-15 00:17:51 +0800

    Exchange 线程间交互数据

--- src/main/java/com/example/demo/springDemo/SpringTest.java
+++ src/main/java/com/example/demo/springDemo/SpringTest.java
@@ -1,6 +1,11 @@
 package com.example.demo.springDemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * @description:
@@ -9,6 +14,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  **/
 public class SpringTest {
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("");
+        BeanFactory beanFactory = new DefaultListableBeanFactory();
+        beanFactory.getBean("");
+        ClassPathResource res = new ClassPathResource("");
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(MyBeanConfig.class);
         applicationContext.register(MyBeanFactoryPostProcessor.class);
